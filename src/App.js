709be022697310192fe2fb20111672c3ffc35542
Jason Cassella <jasoncassella1@gmail.com>
2022-04-25 19:20:55 -0400

    https

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import MovieCard from "./MovieCard";
 import SearchIcon from "./search.svg";
 import "./App.css";
 
-const API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=781e5a5d";
+const API_URL = "https://www.omdbapi.com?apikey=781e5a5d";
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useState("Shrek");
